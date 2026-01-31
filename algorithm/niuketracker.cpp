1772582bd4e9ead02893b5120f989c0deed7bb9b
FRiver-hahaha <1203987745@qq.com>
2026-01-31 22:17:25 +0800

    tracker

--- algorithm/niuketracker.cpp
+++ algorithm/niuketracker.cpp
@@ -1,29 +1,18 @@
 #include <iostream>
 using namespace std;
-#define LL long long
-const LL maxn = 2200000, M = 1000000007, inf = 1LL << 62;
-
 
 int main() {
-    ios:: sync_with_stdio(false);// 去除缓冲区不同步
-    cin.tie(nullptr);// 关闭 cin 和 cout 的绑定
-    LL ar[3];
-    for(int i = 0; i < 3; ++i) {
-        cin >> ar[i];
+    char ar[7];
+    cin >> ar;
+    if(ar[5] >= '3' && ar[5] <= '5') {
+        cout << "spring" << endl;
+    }else if(ar[5] >= '6' && ar[5] <= '8') {
+        cout << "summer" << endl;
+    }else if(ar[5] >= '9' && ar[5] <= '11') {
+        cout << "autumn" << endl;
+    }else if(ar[5] == '12' || ar[5] == '1' || ar[5] == '2') {
+        cout << "winter" << endl;
     }
-
-    for(int i = 0; i < 3; ++i) {
-        for(int j = 0; j < 3 - i - 1; ++j) {
-            if(ar[j] > ar[j + 1]) {
-                LL tmp = ar[j];
-                ar[j] = ar[j + 1];
-                ar[j + 1] = tmp;
-            }
-        }
-    }
-
-    cout << "The maximum number is : " << ar[2] << endl;
-    cout << "The minimum number is : " << ar[0] << endl;
 
     return 0;
 }
