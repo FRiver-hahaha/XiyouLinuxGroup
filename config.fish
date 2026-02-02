9d716b95d4797033e24964cae13d4d4dfc696b4e
FRiver-hahaha <1203987745@qq.com>
2026-02-02 16:26:47 +0800

    config

---
+++ config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    alias gc='google-chrome'
+    alias cl='clash-verge'
+    alias xiyoulinux='cd /home/friver/XiyouLinuxGroup/The_Linux_Programming_Interface'
+    alias xiyougit="git add . && git commit -m $argv && git push"
+    alias mc='cd ~/下载/ && ./HMCL-3.8.2.sh'
+    eval "$(zoxide init fish)"
+end
