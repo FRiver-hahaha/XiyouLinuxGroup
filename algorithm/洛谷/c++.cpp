66dbcb55894304801919dfc0dfb7daed7c25981f
FRiver-hahaha <FRiver-hahaha@github.com>
2026-03-15 19:36:43 +0800

    洛谷题

--- algorithm/洛谷/c++.cpp
+++ algorithm/洛谷/c++.cpp
@@ -1,22 +1,30 @@
-#include <iostream>
+#include <bits/stdc++.h>
 #include <cstring>
 using namespace std;
 #define LL long long
 
+
 int main() {
-    LL n;
-    char name[20];
-    scanf("%lld", &n);
-    LL sum = 0, money = 0;
+    LL n, m;
+    LL level = LLONG_MAX;
+
+    cin >> n >> m;
+
+    vector<LL> A(n);
+    vector<LL> B(n);
+
     for(int i = 0; i < n; ++i) {
-        scanf("%s %lld", name, &money);
-        if(strcmp(name, "Q") == 0) {
-            sum += money;
-        }else {
-            continue;
-        }
+        cin >> A[i];
+        B[i] = A[i] * A[i];
     }
-    printf("%lld", sum);
+
+    sort(B.begin(), B.end());
+
+    for(int i = 0; i + m - 1 < n; ++i) {
+        level = min(level, B[i + m - 1] - B[i]);
+    }
+
+    cout << level << endl;
 
     return 0;
 }
