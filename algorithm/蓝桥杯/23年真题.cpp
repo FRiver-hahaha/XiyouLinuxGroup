20c2d7086147a0a0c561a202000b8d7bf7d8db4c
FRiver-hahaha <1203987745@qq.com>
2026-03-18 21:21:13 +0800

    algorithm:主要练习了一下25年和23年的蓝桥杯大学a组真题的简单题，质数，红黑数，找数

---
+++ algorithm/蓝桥杯/23年真题.cpp
@@ -0,0 +1,40 @@
+
+
+
+        sum1 += arr[i - j - 1];
+        sum2 += arr[j];
+      cnt++;
+      return 0;
+      return 1;
+    }
+    }
+    }
+    }else {
+    arr[i++] = x % 10;
+    for(int j = 0; j < i / 2; j++) {
+    if(isluck(i)) {
+    if(sum1 == sum2) {
+    int sum1 = 0;
+    int sum2 = 0;
+    return 0;
+    x /= 10;
+  }
+  }
+  }
+  }else {
+  cout << cnt << endl;
+  for(long long i = 1; i <= 100000000; ++i) {
+  if (i% 2 != 0) {
+  int arr[9]; 
+  int cnt = 0;
+  int i = 0;
+  return 0;
+  while(x > 0) {
+{
+}
+}
+// 第一题，找数
+#include <iostream>
+int isluck(long long x) {
+int main()
+using namespace std;
---
+++ algorithm/蓝桥杯/25年真题.cpp
@@ -0,0 +1,38 @@
+// // 红黑树
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//   long long m;
+//   cin >> m;
+//   for(int i = 0; i < m; ++i) {
+//     long long r, c;
+//     cin >> r >> c;
+//     long long cnt = 0;
+//     for(int level = r; level > 1; level--) {
+//       if(c % 2 == 0) {
+//         cnt++;
+//       }
+//       c = (c + 1) / 2;
+//     }
+//     if(cnt % 2 == 0) {
+//       cout << "RED" << endl;
+//     }else {
+//       cout << "BLACK" << endl;
+//     }
+//   }
+
+//   return 0;
+// }
+
+// // 质数
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//   cout << 17609 << endl;
+//   return 0;
+// }
