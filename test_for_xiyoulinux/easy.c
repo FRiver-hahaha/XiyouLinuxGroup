7d5c26868d795f28bebacdce272515193b91a654
FRiver-hahaha <1203987745@qq.com>
2026-09-03 21:55:18 +0800

    fest:纳新题目

---
+++ test_for_xiyoulinux/easy.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+    欢迎你加入西邮 Linux 兴趣小组!
+    请你解释一下题目的运行结果
+    考点：printf()返回值、while循环跳出
+*/
+
+int main() {
+  while (1) {
+    printf("Hi! ");
+    if (printf("The 202%d, Welcome to Xiyou Linux Group!\n",
+               printf("guys! "))) {
+      break;
+    }
+  }
+  return 0;
+}
